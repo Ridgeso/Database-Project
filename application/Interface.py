c4517e2084a46306a68f81aa38b6825f2c642568
Ridgeso <nienekpalo@gmail.com>
2024-01-29 17:51:13 +0100

    Set python standard to >=3.9

--- application/Interface.py
+++ application/Interface.py
@@ -5,5 +5,5 @@ from tkinter import ttk
 
 class Viewport(ABC):
     @abstractmethod
-    def __init__(self, frameroot: ttk.Frame) -> None:
+    def __init__(self, frameroot):
         pass
--- utils/Errors.py
+++ utils/Errors.py
@@ -1,9 +1,11 @@
 class PythonVersionError(ValueError):
+    REQUIRED_VERSION = (3, 9)
+
     def __init__(self) -> None:
-        super().__init__("This Project requires Python >= 3.7")
+        super().__init__(f"This Project requires Python >= {self.REQUIRED_VERSION[0]}.{self.REQUIRED_VERSION[1]}")
 
     @staticmethod
     def validate_python() -> None:
         from sys import version_info
-        if version_info < (3, 7):
+        if version_info < PythonVersionError.REQUIRED_VERSION:
             raise PythonVersionError()
